a92153d8c93d082bc12c65c2ffe172f6a83279ed
Maciej Piór <mpior@starfishstorage.com>
2026-02-03 00:51:52 +0100

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/laser-spear/',
   plugins: [
     vue({
       template: { transformAssetUrls },
